4b4a9e62fa3f75aa1159d22cb7ea74f00e6236a5
nongsang <shdtkd37@gmail.com>
2019-05-30 19:23:04 +0900

    5월 30일 IP 주소

--- C#/Basic/001.IP_Address.cs
+++ C#/Basic/001.IP_Address.cs
@@ -16,6 +16,13 @@ namespace CPPPP
 
             IPAddress ipAddr2 = new IPAddress(new byte[] { 202, 179, 177, 21 });    // byte형으로 값을 하나씩 전달해도 된다.
             Console.WriteLine(ipAddr2);
+
+            IPAddress ipAddr3 = IPAddress.Parse("2001:0000:85a3:0042:1000:8a8e:0370:7334"); // IPAddress는 IPv6도 자동으로 해석해 저장할 수 있다.
+            Console.WriteLine(ipAddr3);
+
+            //IPAddress ipAddr4 = IPAddress.Parse("2001:0000:0000:0000:0000:0000:0000:7334"); // 아래와 같은 주소
+            IPAddress ipAddr4 = IPAddress.Parse("2001::7334");  // 2001과 7334모두 0000이라면 ::으로 줄일 수 있다.
+            Console.WriteLine(ipAddr4);
         }
     }
 }
